dd04f84d79c57e513e73632567f82818b0e112be
Fernando Americano <americano@gmail.com>
2018-04-08 21:01:15 -0300

    change google api key to nil to test

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -6,7 +6,7 @@ Geocoder.configure(
   timeout: 10,
   lookup: :google,
   use_https: true,
-  api_key: ENV['GOOGLE_API_KEY'],
+  api_key: nil.  #ENV['GOOGLE_API_KEY'],
   cache: Redis.new(:url => uri),
   always_raise: [
     Geocoder::OverQueryLimitError,
